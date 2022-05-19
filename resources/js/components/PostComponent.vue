164e19c0211d10e3e0af7ffbd7b1e9ac7ab550a3
uservasian <vasilek96@bk.ru>
2022-05-19 16:07:59 +0300

    add vuejs and bootstrap, create first component vuejs

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Hello world!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
